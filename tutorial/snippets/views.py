18a46ff36e34ec9501880ff39504e78eac0ce94a
zlinna <1537216090@qq.com>
2020-01-15 23:49:14 +0800

    add authentication & permissions.
    1. Code snippets are always associated with a creator.
    2. Only authenticated users may create snippets.
    3. Only the creator of a snippet may update or delete it.
    4. Unauthenticated requests should have full read-only access.
    
    Signed-off-by: zlinna <1537216090@qq.com>

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,7 +1,18 @@
+from django.contrib.auth.models import User
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.serializers import UserSerializer, SnippetSerializer
+from snippets.permissions import IsOwnerOrReadOnly
 from rest_framework import generics
+from rest_framework import permissions
 
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class SnippetList(generics.ListCreateAPIView):
     """
@@ -10,6 +21,12 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    # adding required permissions to views
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    # associating Snippets with Users
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
@@ -17,3 +34,4 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
