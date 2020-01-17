f4dd96f46ea901414353c7809e1adf37e9d594fe
zlinna <1537216090@qq.com>
2020-01-17 23:57:48 +0800

    relationships and hyperlinked-apis
    * use a regular function-based view and the @api_view decorator
    * Hyperlinking our API
       1. Using primary keys.
       2. Using hyperlinking between entities.
       3. Using a unique identifying slug field on the related entity.
       4. Using the default string representation of the related entity.
       5. Nesting the related entity inside the parent representation.
       6. Some other custom representation ...
    
    Signed-off-by: zlinna <1537216090@qq.com>

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,9 +2,18 @@ from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.serializers import UserSerializer, SnippetSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework import generics
-from rest_framework import permissions
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
@@ -35,3 +44,11 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
